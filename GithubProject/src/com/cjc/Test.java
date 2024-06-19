9323bd09292fbf1613eecac9509186608a8d4848
surekha2707 <surekha.sc.1997@gmail.com>
2024-06-19 08:09:42 +0530

    Update Test.java

--- GithubProject/src/com/cjc/Test.java
+++ GithubProject/src/com/cjc/Test.java
@@ -2,9 +2,15 @@ package com.cjc;
 
 public class Test 
 {
+	public void m1()
+	{
+		System.out.println("m1 method")
+			}		
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello selenium");
+		Test t=new Test();
+		t.m1();
 		
 	}
 
