db56c139be0b7f083ad09b86e9c3307c55894692
surekha2707 <surekha.sc.1997@gmail.com>
2024-06-20 14:46:59 +0530

    surekha-branch1

--- GithubProject/src/com/cjc/Test.java
+++ GithubProject/src/com/cjc/Test.java
@@ -5,7 +5,11 @@ public class Test
 	public void m1()
 	{
 		System.out.println("m1 method")
-			}		
+			}
+	public void m2()
+	{
+		system.out.println("m2 method")
+			}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello selenium");
